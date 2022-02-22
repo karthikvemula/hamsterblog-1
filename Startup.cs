6f1203a101960d8cec6da4b0a211404252499c26
karthikvemula <44648255+karthikvemula@users.noreply.github.com>
2022-02-22 15:16:32 +0530

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -33,7 +33,11 @@ namespace reactasp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.Run(async (context) =>  
+            {  
+                await context.Response.WriteAsync("Hello World!");  
+            });
+            /*if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -65,7 +69,7 @@ namespace reactasp
                 {
                     spa.UseReactDevelopmentServer(npmScript: "start");
                 }
-            });
+            });*/
         }
     }
 }
